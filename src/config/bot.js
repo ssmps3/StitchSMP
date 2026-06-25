945e18f2faa9ea6563f86a6f9788ad1636176d85
ssmps3 <donutsmpplayer2020@gmail.com>
2026-06-25 16:31:38 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -26,7 +26,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Stitchsmp",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 1,
       },
     ],
   },
